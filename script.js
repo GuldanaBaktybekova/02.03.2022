508d040dc055c5475eaec18f8d236ed69696ba7d
Guldana Baktybekova <guldanabaktybekova10@gmail.com>
2022-03-02 15:15:17 +0600

    Added if to js

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 document.querySelectorAll(".accordion").forEach((accordion) => {
+  const parent = accordion.closest('.accordions');
   accordion.querySelector(".accordion__header").addEventListener("click", function () {
+    if ( parent && parent.classList.contains('accordion--toggle')) {
+      parent.querySelectorAll('.accordion').forEach(accordion => {
+        accordion.classList.remove("accordion--open");
+      });
+    }
       accordion.classList.toggle("accordion--open");
     });
 });
