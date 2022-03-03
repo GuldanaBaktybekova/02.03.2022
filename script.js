c536140c36e142c0176c564d9a012e6680fde94b
Guldana Baktybekova <guldanabaktybekova10@gmail.com>
2022-03-03 21:43:16 +0600

    Added format

--- script.js
+++ script.js
@@ -1,27 +1,26 @@
-document.querySelectorAll('.accordion').forEach(accordion => {
-  const parent = accordion.closest('.accordions');
+document.querySelectorAll(".accordion").forEach((accordion) => {
+  const parent = accordion.closest(".accordions");
 
-  accordion.querySelector('.accordion__header').addEventListener('click', function(){
-    if ( parent && parent.classList.contains('accordions--toggle')) {
-      parent.querySelectorAll('.accordion').forEach(accordion => {
-        accordion.classList.remove("accordion--open");
-      })
-    }
-    accordion.classList.add('accordion--open');
-  });
-  
+  accordion
+    .querySelector(".accordion__header")
+    .addEventListener("click", function () {
+      if (parent && parent.classList.contains("accordions--toggle")) {
+        parent.querySelectorAll(".accordion").forEach((accordion) => {
+          accordion.classList.remove("accordion--open");
+        });
+      }
+      accordion.classList.add("accordion--open");
+    });
 });
 
-document.querySelectorAll('[data-modal]').forEach(trigger => {
-  trigger.addEventListener('click', function(){
-   document.getElementById(trigger.dataset.modal).classList.add('modal--open');
-
+document.querySelectorAll("[data-modal]").forEach((trigger) => {
+  trigger.addEventListener("click", function () {
+    document.getElementById(trigger.dataset.modal).classList.add("modal--open");
   });
-})
+});
 
-document.querySelectorAll('.modal').forEach(modal => {
- modal.querySelector('.modal__window').addEventListener('click', function(){
-   modal.classList.remove('modal--open');
- });
-
-})
+document.querySelectorAll(".modal").forEach((modal) => {
+  modal.querySelector(".modal__window").addEventListener("click", function () {
+    modal.classList.remove("modal--open");
+  });
+});
