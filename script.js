b9fa4d51737df3fa046e50540cb6ef49e96d554d
Guldana Baktybekova <guldanabaktybekova10@gmail.com>
2022-03-03 21:18:00 +0600

    Added arrow func

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-document.querySelectorAll(".accordion").forEach((accordion) => {
+document.querySelectorAll('.accordion').forEach(accordion => {
   const parent = accordion.closest('.accordions');
-  accordion.querySelector(".accordion__header").addEventListener("click", function () {
-    if ( parent && parent.classList.contains('accordion--toggle')) {
+
+  accordion.querySelector('.accordion__header').addEventListener('click', function(){
+    if ( parent && parent.classList.contains('accordions--toggle')) {
       parent.querySelectorAll('.accordion').forEach(accordion => {
         accordion.classList.remove("accordion--open");
-      });
+      })
     }
-      accordion.classList.toggle("accordion--open");
-    });
+    accordion.classList.add('accordion--open');
+  });
+  
 });
